800a93fdf9c1505d1ac9d6e0b879b0ee5c100da9
Azmol-Miah <azmol@azmolmiah.com>
2019-02-05 20:32:35 +0000

    form test

--- js/main.js
+++ js/main.js
@@ -2,25 +2,15 @@ let message;
 let messageClass;
 
 function validateForm(e) {
-  // Get form data
   let name = document.forms["myForm"]["name"].value;
   let email = document.forms["myForm"]["email"].value;
   let message = document.forms["myForm"]["message"].value;
 
   if (name == "" || email == "" || message == "") {
-    // Failed
+    document.getElementById("name").innerText = "Name must be filled out";
+    document.getElementById("email").innerText = "Email must be filled out";
+    document.getElementById("message").innerText = "Message must be filled out";
     e.preventDefault();
-    message = "Please fill in all fields";
-    messageClass = "alert-danger";
-    if (message != "") {
-      document.getElementById("name").classList.add(`alert ${messageClass}`);
-      document.getElementById("email").classList.add(`alert ${messageClass}`);
-      document.getElementById("message").classList.add(`alert ${messageClass}`);
-      document.getElementById("name").innerText = message;
-      document.getElementById("email").innerText = message;
-      document.getElementById("message").innerText = message;
-    }
-  } else {
-    // Passed
+    return false;
   }
 }
