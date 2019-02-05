3ce8c6eca0481f0d909c85429fc6a0123e0d8bf8
Azmol-Miah <azmol@azmolmiah.com>
2019-02-05 20:33:54 +0000

    not returning the fals

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let message;
 let messageClass;
 
-function validateForm(e) {
+function validateForm() {
   let name = document.forms["myForm"]["name"].value;
   let email = document.forms["myForm"]["email"].value;
   let message = document.forms["myForm"]["message"].value;
@@ -10,7 +10,7 @@ function validateForm(e) {
     document.getElementById("name").innerText = "Name must be filled out";
     document.getElementById("email").innerText = "Email must be filled out";
     document.getElementById("message").innerText = "Message must be filled out";
-    e.preventDefault();
+
     return false;
   }
 }
