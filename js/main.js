8038884b0f58bde0e14c669df470924c554b51a4
Azmol-Miah <azmol@azmolmiah.com>
2019-02-05 21:04:32 +0000

    just checking again

--- js/main.js
+++ js/main.js
@@ -3,22 +3,21 @@ let emailAlert = document.getElementById("email");
 let messageAlert = document.getElementById("message");
 let messageClass = "alert alert-danger";
 
+let name = document.forms["myForm"]["name"].value;
+let email = document.forms["myForm"]["email"].value;
+let message = document.forms["myForm"]["message"].value;
+
 function validateForm() {
-  let name = document.forms["myForm"]["name"].value;
-  let email = document.forms["myForm"]["email"].value;
-  let message = document.forms["myForm"]["message"].value;
+  if (name === "" && email === "" && message === "") {
+    // nameAlert.innerText = "Name must be filled out";
+    // emailAlert.innerText = "Email must be filled out";
+    // messageAlert.innerText = "Message must be filled out";
 
-  if (name == "" || email == "" || message == "") {
-    nameAlert.innerText = "Name must be filled out";
-    emailAlert.innerText = "Email must be filled out";
-    messageAlert.innerText = "Message must be filled out";
+    // nameAlert.classList.add(`${messageClass}`);
+    // emailAlert.classList.add(`${messageClass}`);
+    // messageAlert.classList.add(`${messageClass}`);
 
-    nameAlert.classList.add(`${messageClass}`);
-    emailAlert.classList.add(`${messageClass}`);
-    messageAlert.classList.add(`${messageClass}`);
-
+    alert("Notworking");
     return false;
-  } else {
-    return true;
   }
 }
